d6c70e4dde5ea851ed4fd88e1b9770b3ed298a0b
Joao Leal <joaoo.lealc@gmail.com>
2024-02-21 23:00:26 -0300

    feat: Add client app with URLs, views, and templates

---
+++ client/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import (
+    DashboardView
+)
+
+app_name = "client"
+
+urlpatterns = [
+    path("dashboard/", DashboardView.as_view(), name="client-dashboard")
+]
