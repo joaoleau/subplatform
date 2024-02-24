9776913830e34f6ef90ad834336bf98d058718cd
Joao Leal <joaoo.lealc@gmail.com>
2024-02-24 18:35:36 -0300

    feat: Adjust permissions of subs

--- article/views.py
+++ article/views.py
@@ -13,7 +13,13 @@ from .mixins import AuthorPermissionMixin
 from .forms import ArticleForm
 from .models import Article
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from article.models import Article
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from client.mixins import SubscriptionPermissionMixin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse
 
 class CreateArticleView(LoginRequiredMixin, CreateView):
     template_name = "article/article-create.html"
@@ -72,3 +78,47 @@ class DetailArticleView(AuthorPermissionMixin, DetailView):
     model = Article
     context_object_name = "article"
     pk_url_kwarg = "pk"
+
+
+class ListArticleView(SubscriptionPermissionMixin, ListView):
+    template_name = "client/list-article.html"
+    context_object_name = "article_list"
+    model = Article
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                "verbose_name": self.model._meta.verbose_name.title(),
+                "verbose_name_plural": self.model._meta.verbose_name_plural.title(),
+            }
+        )
+        return context
+
+    def get_user_request(self):
+        return get_user_model().objects.get(pk=self.request.user.id)
+
+    def get_redirect_subscription_locked(self):
+        return redirect("client:subscription-locked")
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        user = self.get_user_request()
+        if not user.is_sub():
+            return self.get_redirect_subscription_locked()
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self) -> QuerySet[Any] | None:
+        user = self.get_user_request()
+
+        if user.get_subscription():
+            plan, is_active = user.subscription.get_plan_and_is_active()
+
+            if is_active is False:
+                self.model.objects.none()
+
+            elif plan == "Standard":
+                return super().get_queryset().filter(is_premium=False)
+
+            return super().get_queryset()
+
+        return self.model.objects.none()
