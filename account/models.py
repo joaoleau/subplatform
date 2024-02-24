52251a9ec5ad9828bb96ce06d642a2dde9e7a7e1
Joao Leal <joaoo.lealc@gmail.com>
2024-02-24 12:30:04 -0300

    fix: Fix issues in the account app

--- account/models.py
+++ account/models.py
@@ -6,13 +6,12 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), max_length=100, unique=True)
-    first_name = models.CharField(_("first name"), max_length=100, blank=True)
-    last_name = models.CharField(_("last name"), max_length=100, blank=True)
+    first_name = models.CharField(_("first name"), max_length=100)
+    last_name = models.CharField(_("last name"), max_length=100)
     is_active = models.BooleanField(_("active"), default=True)
     is_staff = models.BooleanField(_("staff"), default=False)
     is_superuser = models.BooleanField(_("superuser"), default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    is_writer = models.BooleanField(_("Are you a writer?"), default=False)
     username = None
 
     USERNAME_FIELD = "email"
--- account/urls.py
+++ account/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("account-management/", AccountManagement.as_view(), name="account-management"),
+    path("account/management/", AccountManagement.as_view(), name="account-management"),
     path("delete/", DeleteAccountView.as_view(), name="account-delete"),
 ]
