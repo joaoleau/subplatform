c56c3ef673e669d5bf3c9083742a060d9ff1402a
João Vitor Leal <joaoo.lealc@gmail.com>
2024-03-14 12:39:43 -0300

    Update database connection settings

--- subplatform/settings.py
+++ subplatform/settings.py
@@ -66,6 +66,10 @@ WSGI_APPLICATION = "subplatform.wsgi.application"
 #     "default": {
 #         "ENGINE": config("DATABASE_ENGINE"),
 #         "NAME": config("DATABASE_NAME"),
+#         "USER": config("DATABASE_USER"),
+#         "PASSWORD": config("DATABASE_PASSWORD"),
+#         "HOST": config("DATABASE_HOST"),
+#         "PORT": config("DATABASE_PORT"),
 #     }
 # }
 
